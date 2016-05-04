56e198a0969cc528e57741b107ca61f25177f498
nanoaguayo <fdaguayo@uc.cl>
2016-05-03 22:27:03 -0300

    Email deleted

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  gmail_username: 'aguayosapunar@gmail.com'
-  gmail_password: 'proyecto2016'
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
