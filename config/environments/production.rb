06a95f08e1c6bf5aa8303db30cfeafdfe4d476b6
nanoaguayo <fdaguayo@uc.cl>
2016-05-29 16:25:09 -0300

    Assets not working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,13 +4,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.serve_static_files = true
+  config.serve_static_assets = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.assets.compile = true
-
-  config.assets.initialize_on_precompile = false
-
-  rake assets:precompile
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -30,7 +26,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
